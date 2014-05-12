4ab43d4934e9b58b4dae5c990b145705f8999303
Thanatermesis <thanatermesis@gmail.com>
2014-05-12 18:10:12 +0200

    my last version of layout which where I worked nicely

--- firmware/keyboard/ergodox/options.h
+++ firmware/keyboard/ergodox/options.h
@@ -22,7 +22,8 @@
 // firmware/main
 // ----------------------------------------------------------------------------
 
-#define  OPT__DEBOUNCE_TIME  5
+//#define  OPT__DEBOUNCE_TIME  5
+#define  OPT__DEBOUNCE_TIME  30
 // in milliseconds
 
 
